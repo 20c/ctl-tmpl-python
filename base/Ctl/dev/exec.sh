95b6de449386ca7709f54dece1207345e50ed06e
Matt Griswold <grizz@20c.com>
2021-08-27 10:37:51 -0500

    move to base dir (#3)
    * use project_name
    
    * add flake8
    
    * update default pyproject
    
    * update templates
    
    * add std precommit hooks
    
    * flip readme position
    
    * typo
    
    * copypasta container dev
    
    * update compose
    
    * use current linters
    
    * skip test data
    
    * updates
    
    * update pyproject
    
    * fix lgtm link
    
    * update compose
    
    * add tox.ini
    
    Co-authored-by: Stefan Pratter <stefan@20c.com>

---
+++ base/Ctl/dev/exec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+service="$1"; shift
+if [[ -z "${service}" ]]; then
+  echo missing service name
+  exit 1
+fi
+
+COMPOSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_NAME=$(basename $(git rev-parse --show-toplevel))
+
+service_id=$(docker-compose -p $PROJECT_NAME -f $COMPOSE_DIR/docker-compose.yml ps -q $service)
+
+if [[ -z "${service_id}" ]]; then
+  echo error finding service id
+  exit 1
+fi
+
+docker exec -it $service_id "$@"
